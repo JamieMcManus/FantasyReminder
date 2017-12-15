24ef0d69cfb3360b29df497e976f06713dc162b2
Jamie McManus <jamiemcmanus21@gmail.com>
2017-12-15 20:42:29 +0000

    Convert to Alarms API

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 
 function show(message) {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+  var time = /(..)(:..)/.exec(new Date());     
+  var hour = time[1] % 12 || 12;               
+  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; 
+
+  //Create new notification
   notification =new Notification(hour + time[2] + ' ' + period, {
     icon: 'premIcon.png',
     body:message
   });
+  //Open Fantasy page if clicked
   notification.onclick = function(){
         window.open('https://fantasy.premierleague.com/');
         window.focus();
@@ -21,13 +24,42 @@ if (!localStorage.isInitialized) {
   localStorage.isInitialized = true; 
 }
 
-//chrome.alarms.onAlarm.addListener()   investigate alarms
+//create both alarms 
+function createAlarm(){
+   var today = new Date();
+   var timestampOne = +new Date(today.getFullYear(), today.getMonth(), today.getDate(), 18, 0, 0, 0); //Fri @ 6pm
+   var timestampTwo= +new Date(today.getFullYear(), today.getMonth(), today.getDate(), 19, 30, 0, 0); //Fri @ 7:30pm
+   
+   if (today.getDay()==5){    // Only create alarm if Fri
+      // Create
+      chrome.alarms.create('firstAlarm', {
+          when: timestampOne
+      });
+
+      chrome.alarms.create('secondAlarm', {
+          when: timestampTwo
+      });
+   }
+
+}
+createAlarm();   // Create function
+
+//Listen for alarms
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    if (alarm.name === 'firstAlarm') {
+         show("Time is almost up...");    //Call show notification functions
+    }
+    else if (alarm.name === 'secondAlarm') {
+         show("Don't forget to make your changes...");     //Call show notification functions
+    }
+});
 
 
+
+/*  Old Code 
 //check time
 function checkTime(){
   if (window.Notification) {
-  
       var today = new Date();
       var d=today.getDay();
       var t=today.getHours();
@@ -46,8 +78,5 @@ function checkTime(){
     }
   } 
 }
-
-
 setInterval(checkTime(), 60000);
-
-
+*/
